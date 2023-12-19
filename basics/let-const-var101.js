08877d85913ec7cb4969fad276d92f4d73ca94b9
sidd851 <154255943+sidd851@users.noreply.github.com>
2023-12-19 17:31:45 +0000

    container config done

---
+++ basics/let-const-var101.js
@@ -0,0 +1,10 @@
+const accountId = 112233
+let accountEmail = "sidd@gmail.com" 
+var accountPassword = "332211"
+accountCity = "delhi"
+let accountState;
+
+
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
