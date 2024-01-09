2043cb2fdaa29d37293fba531f22498e1ee8735b
sidd851 <154255943+sidd851@users.noreply.github.com>
2024-01-09 11:39:37 +0000

    stack and string

---
+++ basics/stack-heap.js
@@ -0,0 +1,26 @@
+//stack and primitive
+let bot = "deni"
+
+nongamer = bot 
+
+nongamer = "kuchbhi"
+
+ console.log(bot);
+ console.log(nongamer);
+
+
+//heap and nonprimitive
+
+let gamer1 = {
+    username: "denisim#bot" , 
+    aim: "noob"          
+
+}
+
+let gamer2 = gamer1
+
+//gamer2.aim = "pro"
+
+
+console.log(gamer1);
+console.log(gamer2);
---
+++ basics/string.js
@@ -0,0 +1,21 @@
+const gamename = "denisim"
+
+//console.log(gamename.charAt(4));
+//console.log(gamename.indexOf('i'));
+
+const newgame = gamename.slice(1,5)
+const newgame2 = gamename.slice(-7,5)
+console.log(newgame.toUpperCase());
+console.log(newgame2.bold());
+
+
+
+//console.log("denisim is a very \n big bot ");
+
+const url = "//htttps%20kaj.com"
+console.log(url.replace('%20','bot'));
+
+
+
+
+
