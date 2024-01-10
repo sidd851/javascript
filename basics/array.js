1c42c0bdc77c83b30923c642d8ca3c021da0186f
sidd851 <154255943+sidd851@users.noreply.github.com>
2024-01-10 10:56:23 +0000

    areyyyyy+

---
+++ basics/array.js
@@ -0,0 +1,68 @@
+const arr = [0,1,2,3,4,5,6,7,8,9,10]
+
+let arr2 = [11,22,33,44,55,]
+
+//console.log(arr.at(3));
+
+//console.log(arr.concat(arr2));
+
+//console.log(arr);
+//console.log(arr.copyWithin(2,8));
+
+//*console.log(arr.every(1:));
+
+//console.log(arr.fill(4,2,7));
+
+//*console.log(arr.filter());
+
+
+//*console.log(arr.find());
+
+//*console.log(arr.forEach(arr2[1,2,3,4]));
+
+let string = "sidd"
+
+//*console.log(arr.join(string));
+
+//console.log(arr.pop());
+
+//arr.push(8512075907)
+
+//console.log(arr.push());
+
+//console.log(arr);
+
+//*console.log(arr.reduce());
+
+//console.log(arr.reverse());
+
+//arr.shift()
+
+//console.log(arr.shift());
+
+//console.log(arr);
+
+//console.log(arr.slice(2,9));
+
+//*console.log(arr.some());
+
+let arr3  = [2,5,1,8,3,6,9,44,88,]
+
+//console.log(arr3.sort(4,8('2<5')));
+
+//console.log(arr.toLocaleString());
+
+//console.log(arr.toReversed()); (NOT A FUNCITON)
+
+//console.log(arr.unshift(66));
+//console.log(arr);
+
+//console.log(arr.with); // (NOT A FUNTION)
+
+
+
+
+
+
+
+
---
+++ basics/nums-maths.js
@@ -0,0 +1,6 @@
+const num = 112233.4584
+
+console.log(num.toPrecision(7));
+
+
+
